ce0f0b84b16d0edee1d803df36f65a0407733f41
xiaocuixt <cjw624923@126.com>
2015-01-24 21:42:00 +0800

    后台样式改写

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  http_basic_authenticate_with name: "admin", password: "cjw15963793254"
   layout 'admin'
   protect_from_forgery with: :exception
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     end
   end
   namespace :admin do
+    root 'home#index'
     resources :articles
   end
   # The priority is based upon order of creation: first created -> highest priority.
